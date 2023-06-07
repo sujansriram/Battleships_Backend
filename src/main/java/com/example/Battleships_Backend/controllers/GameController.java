8a0c9c2dd9136735555b2f4a505f1342ee5b311f
Sujan Sriram <sujansriram9@gmail.com>
2023-06-07 16:10:29 +0100

    created routes in game controller

--- src/main/java/com/example/Battleships_Backend/controllers/GameController.java
+++ src/main/java/com/example/Battleships_Backend/controllers/GameController.java
@@ -1,12 +1,17 @@
 package com.example.Battleships_Backend.controllers;
 
+import com.example.Battleships_Backend.models.Cell;
+import com.example.Battleships_Backend.models.Game;
+import com.example.Battleships_Backend.models.Grid;
+import com.example.Battleships_Backend.models.Reply;
 import com.example.Battleships_Backend.services.CellService;
 import com.example.Battleships_Backend.services.GameService;
 import com.example.Battleships_Backend.services.GridService;
 import com.example.Battleships_Backend.services.ShipService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value ="/games")
@@ -25,5 +30,40 @@ public class GameController {
     CellService cellService;
 
 
+    @GetMapping
+    public ResponseEntity<Game> getGame(){
+        Game game = gameService.getGame();
+        return new ResponseEntity<>(game, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> createGame(){
+        Game game = gameService.createGame();
+        return new ResponseEntity<>(game, HttpStatus.CREATED);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Game> updateGridSetup(@RequestBody(required = false) Grid grid){
+        Game game;
+        if (grid != null){
+           game = gameService.addSetupGrid(grid);
+        } else {
+            game = gameService.startGame();
+        }
+        return new ResponseEntity<>(game, HttpStatus.OK);
+
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Reply> handleTurn(@PathVariable("id") Long id, @RequestBody Cell cell){
+        Reply reply = gameService.handleTurn(cell);
+        return new ResponseEntity<>(reply, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Game> resetGame(){
+        Game game = gameService.resetGame();
+        return new ResponseEntity<>(game, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/example/Battleships_Backend/services/GameService.java
+++ src/main/java/com/example/Battleships_Backend/services/GameService.java
@@ -1,5 +1,9 @@
 package com.example.Battleships_Backend.services;
 
+import com.example.Battleships_Backend.models.Cell;
+import com.example.Battleships_Backend.models.Game;
+import com.example.Battleships_Backend.models.Grid;
+import com.example.Battleships_Backend.models.Reply;
 import com.example.Battleships_Backend.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,21 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    public Game resetGame() {
+    }
+
+    public Reply handleTurn(Cell cell) {
+    }
+
+    public Game getGame() {
+    }
+
+    public Game createGame() {
+    }
+
+    public Game addSetupGrid(Grid grid) {
+    }
+
+    public Game startGame() {
+    }
 }
