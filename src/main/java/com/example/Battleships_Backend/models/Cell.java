c3f88c740721b3a53d60f339e459b34e40324d3d
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-06-07 12:18:51 +0100

    started ship and cell model

--- src/main/java/com/example/Battleships_Backend/models/Cell.java
+++ src/main/java/com/example/Battleships_Backend/models/Cell.java
@@ -1,4 +1,70 @@
 package com.example.Battleships_Backend.models;
 
 public class Cell {
+
+    private Long id;
+    private int xCoordinate;
+    private int yCoordinate;
+    private boolean hasBeenHit;
+    private Ship ship;
+    private Grid grid;
+
+    public Cell(int xCoordinate, int yCoordinate, Grid grid){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.hasBeenHit = false;
+        this.ship = null;
+        this.grid = grid;
+    }
+
+    public Cell(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public boolean isHasBeenHit() {
+        return hasBeenHit;
+    }
+
+    public void setHasBeenHit(boolean hasBeenHit) {
+        this.hasBeenHit = hasBeenHit;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+
+    public void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    public Grid getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Grid grid) {
+        this.grid = grid;
+    }
 }
--- src/main/java/com/example/Battleships_Backend/models/Ship.java
+++ src/main/java/com/example/Battleships_Backend/models/Ship.java
@@ -1,4 +1,64 @@
 package com.example.Battleships_Backend.models;
 
+
+import java.util.ArrayList;
+
 public class Ship {
+    private Long id;
+    private String name;
+    private int size;
+    private boolean hasSunk;
+    private ArrayList<Cell> cells;
+
+
+    public Ship(String name, int size){
+        this.name = name;
+        this.size = size;
+        this.hasSunk = false;
+        this.cells = new ArrayList<>();
+    }
+
+    public Ship(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public boolean isHasSunk() {
+        return hasSunk;
+    }
+
+    public void setHasSunk(boolean hasSunk) {
+        this.hasSunk = hasSunk;
+    }
+
+    public ArrayList<Cell> getCells() {
+        return cells;
+    }
+
+    public void setCells(ArrayList<Cell> cells) {
+        this.cells = cells;
+    }
 }
