e4d0bda3a45eb6cfb550df44f6f22c449a982505
Sujan Sriram <sujansriram9@gmail.com>
2023-06-07 11:05:21 +0100

    created battleships backend

---
+++ src/main/java/com/example/Battleships_Backend/BattleshipsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Battleships_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipsBackendApplication.class, args);
+	}
+
+}
