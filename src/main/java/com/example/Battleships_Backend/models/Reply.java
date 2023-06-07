2ac8b5fece4b7deee1b1a40771840ad3e2359b48
Tharahan Tharmaraja <tharahan04@hotmail.com>
2023-06-07 12:48:37 +0100

    started reply model

--- src/main/java/com/example/Battleships_Backend/models/Reply.java
+++ src/main/java/com/example/Battleships_Backend/models/Reply.java
@@ -2,4 +2,33 @@ package com.example.Battleships_Backend.models;
 
 public class Reply {
 
+
+    private Game game;
+    private Cell cell;
+
+
+    public Reply(Game game, Cell cell){
+        this.game = game;
+        this.cell = cell;
+    }
+
+    public Reply(){
+    }
+
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Cell getCell() {
+        return cell;
+    }
+
+    public void setCell(Cell cell) {
+        this.cell = cell;
+    }
 }
